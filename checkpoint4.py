da4695fe75528d3bc53bbf4ce6ebf125382d186f
justin <justinchenchen673@gmail.com>
2021-04-14 17:30:34 -0400

    check4 and 5

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,29 @@
+import pprint
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 client = MongoClient()
 
+def fetchAll(collection):
+    for doc in collection.find():
+        pprint.pprint(doc)
+
+def fetchOne(collection):
+    pprint.pprint(collection.find_one())
+
+def fetchSpecific(key, value, collection):
+    pprint.pprint(collection.find_one({key: value}))
+
+def fetchById(id, collection):
+    pprint.pprint(collection.find_one({"_id": ObjectId(id)}))
+
+def insertRecord(query, collection):
+    pprint.pprint(collection.insert_one(query))
+
 if __name__ == '__main__':
-    print("Modify me")
+    db = client["mongo_db_lab"]
+    defs = db["definitions"]
+    #fetchAll(defs)
+    #fetchOne(defs)
+    #fetchById("56fe9e22bad6b23cde07b949", defs)
+    #insertRecord({"word": "Bubly", "definition": "Greatest sparkling water"}, defs)
+    #fetchSpecific("word", "Bubly", defs)
--- checkpoint5.py
+++ checkpoint5.py
@@ -1,14 +1,30 @@
+import datetime
+from random import randrange
+import pprint
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 client = MongoClient()
 
-
 def random_word_requester():
-    '''
-    This function should return a random word and its definition and also
-    log in the MongoDB database the timestamp that it was accessed.
-    '''
-    return
+    # Connection to db
+    db = client["mongo_db_lab"]
+    collection = db["definitions"]
 
+    # Get random document
+    randIndex = randrange(collection.count_documents({}))
+    count = 0
+    for doc in collection.find():
+        if (randIndex == count):
+            # Add date to the document
+            dt = datetime.datetime.isoformat(datetime.datetime.utcnow())
+            if ("dates" not in doc):
+                doc["dates"] = []
+            doc["dates"].append(dt)
+            collection.update_one({"word": doc["word"]}, {"$set": doc})
+
+            return doc
+
+        count += 1
 
 if __name__ == '__main__':
-    print(random_word_requester())
+    pprint.pprint(random_word_requester())
